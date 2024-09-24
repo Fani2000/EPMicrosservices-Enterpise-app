4eb3739d174b1d1e820bd02b21219b25ffd436fc
Fani Keorapetse <keorapetse.fani@dimensiondata.com>
2024-09-24 22:57:35 +0200

    Updated the Catalog microservice

---
+++ src/Services/Catelog/Catalog.Api/Products/CreateProduct/CreateProductEndpoint.cs
@@ -0,0 +1,28 @@
+﻿
+namespace Catalog.Api.Products.CreateProduct;
+
+public record CreateProductRequest(string Name, List<string> Category, string Descripion, string ImageFile, decimal Price);
+public record CreateProduct(Guid Id);
+
+public class CreateProductEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapPost("/products", async (CreateProductRequest request, ISender sender) =>
+        {
+            var command = request.Adapt<CreateProductCommand>();
+
+            var result = await sender.Send(command);
+
+            var response = result.Adapt<CreateProductResult>();
+
+            return Results.Created($"/products/{response.Id}", response);
+        })
+        .WithName("CreateProduct")
+        .Produces<CreateProductResult>(StatusCodes.Status201Created)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithSummary("Create Product")
+        .WithDescription("Create product");
+
+    }
+}
